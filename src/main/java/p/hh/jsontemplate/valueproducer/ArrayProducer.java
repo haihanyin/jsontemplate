cc50d4251bda5b0f3542d26a128df4ce3afc37c8
haihanyin <iamhaihan@gmail.com>
2019-01-29 21:24:48 +0100

    start to support array

--- src/main/java/p/hh/jsontemplate/valueproducer/ArrayProducer.java
+++ src/main/java/p/hh/jsontemplate/valueproducer/ArrayProducer.java
@@ -1,44 +1,26 @@
-//package p.hh.jsontemplate.valueproducer;
-//
-//import java.util.List;
-//import java.util.Map;
-//import java.util.function.Function;
-//import java.util.function.IntFunction;
-//import java.util.stream.Collectors;
-//import java.util.stream.IntStream;
-//
-//public class ArrayProducer extends AbstractValueProducer implements IArrayProducer {
-//
-//    private final static int SIZE = 3;
-//
-//    ArrayProducer() {
-//
-//    }
-//
-//    public <T> List<T> produce(Map<String, String> arrayParamMap,
-//                               IValueProducer<T> valueProducer,
-//                               String elementValue) {
-//        Integer size = getIntegerParam(arrayParamMap, "size", SIZE);
-//        return produceValueArray(size, valueProducer::produce, elementValue);
-//    }
-//
-//    public <T> List<T> produce(Map<String, String> arrayParamMap,
-//                               IValueProducer<T> valueProducer,
-//                               List<String> elementOptions) {
-//        Integer size = getIntegerParam(arrayParamMap, "size", SIZE);
-//        return produceValueArray(size, valueProducer::produce, elementOptions);
-//    }
-//
-//    public <T> List<T> produce(Map<String, String> arrayParamMap,
-//                               IValueProducer<T> valueProducer,
-//                               Map<String, String> elementParamMap) {
-//        Integer size = getIntegerParam(arrayParamMap, "size", SIZE);
-//        return produceValueArray(size, valueProducer::produce, elementParamMap);
-//    }
-//
-//    private <E, T> List<T> produceValueArray(int size, Function<E, T> function, E expr) {
-//        return IntStream.range(0, size).mapToObj(i -> {
-//            return function.apply(expr);
-//        }).collect(Collectors.toList());
-//    }
-//}
+package p.hh.jsontemplate.valueproducer;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ArrayProducer implements IArrayProducer {
+
+    @Override
+    public List produce(Map<String, String> mapParam, Supplier supplier) {
+        String sizeStr = mapParam.remove("size");
+        if (sizeStr != null) {
+            int size = Integer.parseInt(sizeStr);
+        }
+        for
+    }
+
+    @Override
+    public List produce(Map<String, String> mapParam, List<Supplier> valueList) {
+        return null;
+    }
+}
--- src/main/java/p/hh/jsontemplate/valueproducer/IArrayProducer.java
+++ src/main/java/p/hh/jsontemplate/valueproducer/IArrayProducer.java
@@ -2,9 +2,9 @@ package p.hh.jsontemplate.valueproducer;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 
-public interface IArrayProducer<T> {
-    T produce(Map<String, String> arrayMapParam, IValueProducer<T> elementProducer, String value);
-    T produce(Map<String, String> arrayMapParam, IValueProducer<T> elementProducer, List<String> valueList);
-    T produce(Map<String, String> arrayMapParam, IValueProducer<T> elementProducer, Map<String, String> paramMap);
+public interface IArrayProducer {
+    List produce(Map<String, String> mapParam, Supplier supplier);
+    List produce(Map<String, String> mapParam, List<Supplier> valueList);
 }
